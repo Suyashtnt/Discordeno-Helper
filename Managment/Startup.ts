6ad3e01dbdf97915b1c45a3b9b38ccad55960350
a person <suyashtnt@gmail.com>
2020-09-27 21:08:19 +0200

    better logging

--- Managment/Startup.ts
+++ Managment/Startup.ts
@@ -1,6 +1,8 @@
 import * as deps from "../deps.ts";
 import { commands } from "../Storage/commands.ts";
-
+import { Logger } from "https://deno.land/x/optic/mod.ts";
+import { cache } from "https://x.nest.land/Discordeno@9.0.1/src/utils/cache.ts";
+const logger = new Logger();
 /**
  * Starts up your bot
  * @param token Your Bot Token
@@ -21,13 +23,25 @@ export const startup = (token: string, prefix: string) => {
         if (msg.content.startsWith(prefix)) {
           commands.map((cmd) => {
             if (cmd.command == CommandName) {
-              console.log(`running ${cmd.command}`);
+              logger.info(
+                `running ${cmd.command} in ${
+                  cache.guilds.get(msg.guildID)?.name
+                }(channel is ${cache.channels.get(msg.channelID)?.name}) for ${
+                  msg.author.username
+                }`
+              );
               cmd.runs(msg, Args);
             } else if (
               cmd.aliases != undefined &&
               arrayContains(CommandName, cmd.aliases)
             ) {
-              console.log(`running ${cmd.command}`);
+              logger.info(
+                `running ${cmd.command} in ${
+                  cache.guilds.get(msg.guildID)?.name
+                }(channel is ${cache.channels.get(msg.channelID)?.name}) for ${
+                  msg.author.username
+                }`
+              );
               cmd.runs(msg, Args);
             }
           });
